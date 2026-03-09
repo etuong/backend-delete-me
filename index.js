f9aa809fdf592d2fdc6260731ddca37169e280cd
etuong <etuong@gmail.com>
2026-03-09 16:22:07 -0700

    obfuscate secrets to dotenv file

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const mysql = require("mysql2/promise");
 
 app.use(express.json());
 app.use(cors());
+require("dotenv").config();
 
 const dbConfig = {
-  host: "localhost",
-  user: "user",
-  password: "password",
-  database: "roster"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 }
 
 async function executeQuery(query, params = []) {
